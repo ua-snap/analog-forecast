6bf707d5208902ac1fa173afab0a68e0b56539d8
kyleredilla <kmredilla@alaska.edu>
2023-03-02 12:30:24 -0900

    clean up notebook for checking and aggregating Nome dates profiling results

--- analog_forecast.py
+++ analog_forecast.py
@@ -138,8 +138,6 @@ def rmse(da, ref_da):
     Returns:
         rmse_da (xarrya.DataArray): data array where each time step is the RMSE between da at that same time step and ref_da
     """
-    # rmse_da = np.sqrt(((da - ref_da) ** 2).mean(axis=(1, 2)))
-    
     rmse_arr = np.sqrt(np.apply_over_axes(np.nanmean, (da - ref_da) ** 2, axes=[1,2]).squeeze())
     rmse_da = xr.DataArray(
         data=rmse_arr,
